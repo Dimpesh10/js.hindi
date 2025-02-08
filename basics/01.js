40c1d73abc2abfc795c741e896e78f71f755ffc3
Dimpesh10 <ramchandanidimpesh123@gmail.com>
2025-02-08 11:27:31 +0000

    datatype

---
+++ basics/01.js
@@ -0,0 +1,9 @@
+const accountId = 1234;
+let userPassword = "hello world";
+var forgetPassword = "forget passsword";
+
+console.table([accountId,userPassword,forgetPassword]);
+/*
+do not use var keyword to declare variables 
+because of issue block scope and functional scope
+*/
