91db3bcb289520939a5ee0815bffda43cdd9145e
Dimpesh10 <ramchandanidimpesh123@gmail.com>
2025-02-08 11:42:46 +0000

    conversion

---
+++ basics/03.js
@@ -0,0 +1,26 @@
+let score = "33";
+
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber = Number("33adfasd");
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber)//be carefull while converting as it can be a Nan 
+
+// "33" =>33
+// "string" =>NaN(Not a number)
+// true=>1 and false =>0
+// Null=>0 
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1=>true and 0=>false
+// "" =>false
+// "dimpesh" =>true
+
+
+
+
+
