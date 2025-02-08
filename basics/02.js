111ca90ce526b4b48d8cba74140bb8910891f876
Dimpesh10 <ramchandanidimpesh123@gmail.com>
2025-02-08 11:25:56 +0000

    datatyoe

---
+++ basics/02.js
@@ -0,0 +1,20 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3,3) we are using node js not browser
+
+console.log(3+3);//code rediatebly is very improtant
+console.log("hitesh");
+// different datatypes
+/*
+so the first is the
+Null=>standalone value
+undefined=>not define
+symbol=>unique
+boolean=>true/false
+number
+bigInt
+string
+
+*/
+
+console.log(typeof null)
